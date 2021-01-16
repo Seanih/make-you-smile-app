5a9c1f501c415e2cb02ce5e058e5e4e6d88159c2
Sean Anih <sean.anih@gmail.com>
2021-01-16 01:19:27 -0500

    added API functionality; finishing loading screen/wheel

--- src/components/JokeArea.js
+++ src/components/JokeArea.js
@@ -1,19 +1,41 @@
 import React, { useState } from 'react';
 import '../styles/JokeArea.scss';
+import axios from 'axios';
+import spinner from '../images/spinner.gif';
 
 const JokeArea = () => {
 	const [showJoke, setShowJoke] = useState(false);
 	const [joke, setJoke] = useState('');
+	const [loading, setLoading] = useState(false);
+
+	const fetchJoke = () => {
+		setLoading(true);
+		axios
+			.get(
+				'https://v2.jokeapi.dev/joke/Miscellaneous,Dark,Pun,Spooky?blacklistFlags=racist,explicit&type=single'
+			)
+			.then(response => {
+				setLoading(!loading);
+				setShowJoke(true);
+				setJoke(response.data.joke);
+			})
+			.catch(error => {
+				setLoading(false);
+				console.log(error);
+			});
+	};
+
 	return (
 		<main className='body-container'>
 			<div className='joke-box'>
 				<h1>
 					{showJoke
 						? joke
-						: 'Have you smiled today yet? Well 🤔... what are you waiting for? Click the button!!'}
+						: 'Have you smiled today yet? Well 🤔... What are you waiting for? Click the button!!'}
 				</h1>
-
-				<button className='joke-button'>Grab a joke!</button>
+				<button className='joke-button' onClick={fetchJoke}>
+					{joke ? 'Get another joke!' : 'Make me smile...'}
+				</button>
 			</div>
 		</main>
 	);
